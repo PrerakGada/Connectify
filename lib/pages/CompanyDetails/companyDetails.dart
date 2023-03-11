c093afe69f57ad7ced4360151e3a0366c7912f67
Pranil <awessssomepro@gmail.com>
2023-03-11 20:16:44 +0530

    video conferencing works(most probably)

--- lib/pages/CompanyDetails/companyDetails.dart
+++ lib/pages/CompanyDetails/companyDetails.dart
@@ -3,6 +3,7 @@ import 'package:connectify/theme/typography.dart';
 import 'package:flutter/material.dart';
 // import 'package:connectify/widgets/curved_shape.dart';
 import 'package:connectify/theme/app_colors.dart';
+import 'package:connectify/pages/VideoConferencing/video_conferencing.dart';
 import 'package:connectify/theme/typography.dart';
 
 class CompanyDetails extends StatefulWidget {
@@ -18,71 +19,6 @@ class _CompanyDetailsState extends State<CompanyDetails> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      //     child: Scaffold(
-      //   // extendBodyBehindAppBar: true,
-      //   body: SafeArea(
-      //     top: false,
-      //     bottom: false,
-      //     child: Stack(
-      //       children: [
-      //         Container(
-      //           decoration: const BoxDecoration(color: Colors.red),
-      //           child: Image.network(
-      //               'https://wallpapershome.com/images/pages/pic_v/11331.jpg'),
-      //         ),
-      //         Align(
-      //           alignment: Alignment.bottomCenter,
-      //           child: SingleChildScrollView(
-      //             child: Container(
-      //               width: MediaQuery.of(context).size.width,
-      //               child: Padding(
-      //                 padding: const EdgeInsets.all(20),
-      //                 child: Column(
-      //                   // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //                   crossAxisAlignment: CrossAxisAlignment.start,
-      //                   children: [
-      //                     Center(
-      //                       child: Image.network(
-      //                         height: 80,
-      //                         width: 80,
-      //                         // width: constraints.maxWidth,
-      //                         fit: BoxFit.fitWidth,
-      //                         widget.details.companyPhoto,
-      //                       ),
-      //                     ),
-      //                     SizedBox(
-      //                       height: 20,
-      //                     ),
-      //                     Center(
-      //                       child: Text(
-      //                         widget.details.title,
-      //                         style: textTheme.headlineLarge,
-      //                       ),
-      //                     ),
-      //                     SizedBox(
-      //                       height: 40,
-      //                     ),
-      //                     Text("Description", style: textTheme.headlineMedium),
-      //                     SizedBox(
-      //                       height: 10,
-      //                     ),
-      //                     Text(
-      //                       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      //                       style: textTheme.bodyLarge!.copyWith(
-      //                         color: Colors.grey,
-      //                         height: 1.5,
-      //                       ),
-      //                     ),
-      //                   ],
-      //                 ),
-      //               ),
-      //             ),
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-      // )
       child: Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading: false,
@@ -316,6 +252,10 @@ class _CompanyDetailsState extends State<CompanyDetails> {
           backgroundColor: AppColors.primary,
           onPressed: () {
             // Routemaster.of(context).push('/payment');
+
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return VideoConferencing();
+            }));
           },
           isExtended: true,
           materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
---
+++ lib/pages/VideoConferencing/video_conferencing.dart
@@ -0,0 +1,320 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hmssdk_flutter/hmssdk_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class VideoConferencing extends StatelessWidget {
+  const VideoConferencing({super.key});
+
+  Future<bool> getPermissions() async {
+    if (Platform.isIOS) return true;
+    await Permission.camera.request();
+    await Permission.microphone.request();
+    await Permission.bluetoothConnect.request();
+
+    while ((await Permission.camera.isDenied)) {
+      await Permission.camera.request();
+    }
+    while ((await Permission.microphone.isDenied)) {
+      await Permission.microphone.request();
+    }
+    while ((await Permission.bluetoothConnect.isDenied)) {
+      await Permission.bluetoothConnect.request();
+    }
+    return true;
+  }
+
+// UI to render join screen
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        child: Center(
+          child: ElevatedButton(
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+            // Function to push to meeting page
+            onPressed: () async {
+              await getPermissions();
+              Navigator.push(
+                context,
+                CupertinoPageRoute(builder: (_) => const MeetingPage()),
+              );
+            },
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              child: Text(
+                'Join',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MeetingPage extends StatefulWidget {
+  const MeetingPage({super.key});
+
+  @override
+  State<MeetingPage> createState() => _MeetingPageState();
+}
+
+class _MeetingPageState extends State<MeetingPage>
+    implements HMSUpdateListener {
+  //SDK
+  late HMSSDK hmsSDK;
+
+  // Variables required for joining a room
+  String authToken =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2Nlc3Nfa2V5IjoiNjQwYzg2MDU1Mzc2OWVjZDFkYTI2MGFkIiwicm9vbV9pZCI6IjY0MGM4OWZiNzk1ZWFhZDNlYjY3MmRhYiIsInVzZXJfaWQiOiJqa3hobG9qYSIsInJvbGUiOiJndWVzdCIsImp0aSI6IjJmOGZmNzA4LWE4NjQtNDAyNC1iYjA5LWRjOWEyY2QwM2Q2OCIsInR5cGUiOiJhcHAiLCJ2ZXJzaW9uIjoyLCJleHAiOjE2Nzg2MzAwNTJ9.5LOhVaTEzybotUGO967FTZ2BZHBOwl9tVc10L2LRaus";
+  String userName = "test_user";
+
+  // Variables required for rendering video and peer info
+  HMSPeer? localPeer, remotePeer;
+  HMSVideoTrack? localPeerVideoTrack, remotePeerVideoTrack;
+
+  // Initialize variables and join room
+  @override
+  void initState() {
+    super.initState();
+    initHMSSDK();
+  }
+
+  void initHMSSDK() async {
+    hmsSDK = HMSSDK();
+    await hmsSDK.build(); // ensure to await while invoking the `build` method
+    hmsSDK.addUpdateListener(listener: this);
+    hmsSDK.join(config: HMSConfig(authToken: authToken, userName: userName));
+  }
+
+  // Clear all variables
+  @override
+  void dispose() {
+    remotePeer = null;
+    remotePeerVideoTrack = null;
+    localPeer = null;
+    localPeerVideoTrack = null;
+    super.dispose();
+  }
+
+  // Called when peer joined the room - get current state of room by using HMSRoom obj
+  @override
+  void onJoin({required HMSRoom room}) {
+    room.peers?.forEach((peer) {
+      if (peer.isLocal) {
+        localPeer = peer;
+        if (peer.videoTrack != null) {
+          localPeerVideoTrack = peer.videoTrack;
+        }
+        if (mounted) {
+          setState(() {});
+        }
+      }
+    });
+  }
+
+  // Called when there's a peer update - use to update local & remote peer variables
+  @override
+  void onPeerUpdate({required HMSPeer peer, required HMSPeerUpdate update}) {
+    switch (update) {
+      case HMSPeerUpdate.peerJoined:
+        if (!peer.isLocal) {
+          if (mounted) {
+            setState(() {
+              remotePeer = peer;
+            });
+          }
+        }
+        break;
+      case HMSPeerUpdate.peerLeft:
+        if (!peer.isLocal) {
+          if (mounted) {
+            setState(() {
+              remotePeer = null;
+            });
+          }
+        }
+        break;
+      case HMSPeerUpdate.networkQualityUpdated:
+        return;
+      default:
+        if (mounted) {
+          setState(() {
+            localPeer = null;
+          });
+        }
+    }
+  }
+
+  // Called when there's a track update - use to update local & remote track variables
+  @override
+  void onTrackUpdate(
+      {required HMSTrack track,
+      required HMSTrackUpdate trackUpdate,
+      required HMSPeer peer}) {
+    if (track.kind == HMSTrackKind.kHMSTrackKindVideo) {
+      switch (trackUpdate) {
+        case HMSTrackUpdate.trackRemoved:
+          if (mounted) {
+            setState(() {
+              peer.isLocal
+                  ? localPeerVideoTrack = null
+                  : remotePeerVideoTrack = null;
+            });
+          }
+          return;
+        default:
+          if (mounted) {
+            setState(() {
+              peer.isLocal
+                  ? localPeerVideoTrack = track as HMSVideoTrack
+                  : remotePeerVideoTrack = track as HMSVideoTrack;
+            });
+          }
+      }
+    }
+  }
+
+  // More callbacks - no need to implement for quickstart
+  @override
+  void onAudioDeviceChanged(
+      {HMSAudioDevice? currentAudioDevice,
+      List<HMSAudioDevice>? availableAudioDevice}) {}
+
+  @override
+  void onChangeTrackStateRequest(
+      {required HMSTrackChangeRequest hmsTrackChangeRequest}) {}
+
+  @override
+  void onHMSError({required HMSException error}) {}
+
+  @override
+  void onMessage({required HMSMessage message}) {}
+
+  @override
+  void onReconnected() {}
+
+  @override
+  void onReconnecting() {}
+
+  @override
+  void onRemovedFromRoom(
+      {required HMSPeerRemovedFromPeer hmsPeerRemovedFromPeer}) {}
+
+  @override
+  void onRoleChangeRequest({required HMSRoleChangeRequest roleChangeRequest}) {}
+
+  @override
+  void onRoomUpdate({required HMSRoom room, required HMSRoomUpdate update}) {}
+
+  @override
+  void onUpdateSpeakers({required List<HMSSpeaker> updateSpeakers}) {}
+
+  // Widget to render a single video tile
+  Widget peerTile(Key key, HMSVideoTrack? videoTrack, HMSPeer? peer) {
+    return Container(
+      key: key,
+      child: (videoTrack != null && !(videoTrack.isMute))
+          // Actual widget to render video
+          ? HMSVideoView(
+              track: videoTrack,
+            )
+          : Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue.withAlpha(4),
+                  shape: BoxShape.circle,
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.blue,
+                      blurRadius: 20.0,
+                      spreadRadius: 5.0,
+                    ),
+                  ],
+                ),
+                child: Text(
+                  peer?.name.substring(0, 1) ?? "D",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+    );
+  }
+
+  // Widget to render grid of peer tiles and a end button
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      // Used to call "leave room" upon clicking back button [in android]
+      onWillPop: () async {
+        hmsSDK.leave();
+        Navigator.pop(context);
+        return true;
+      },
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Stack(
+            children: [
+              // Grid of peer tiles
+              Container(
+                height: MediaQuery.of(context).size.height,
+                child: GridView(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      mainAxisExtent: (remotePeerVideoTrack == null)
+                          ? MediaQuery.of(context).size.height
+                          : MediaQuery.of(context).size.height / 2,
+                      crossAxisCount: 1),
+                  children: [
+                    if (remotePeerVideoTrack != null && remotePeer != null)
+                      peerTile(
+                          Key(remotePeerVideoTrack?.trackId ?? "" "mainVideo"),
+                          remotePeerVideoTrack,
+                          remotePeer),
+                    peerTile(
+                        Key(localPeerVideoTrack?.trackId ?? "" "mainVideo"),
+                        localPeerVideoTrack,
+                        localPeer)
+                  ],
+                ),
+              ),
+              // End button to leave the room
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: RawMaterialButton(
+                  onPressed: () {
+                    hmsSDK.leave();
+                    Navigator.pop(context);
+                  },
+                  elevation: 2.0,
+                  fillColor: Colors.red,
+                  padding: const EdgeInsets.all(15.0),
+                  shape: const CircleBorder(),
+                  child: const Icon(
+                    Icons.call_end,
+                    size: 25.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
