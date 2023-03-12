1445a994d0511769865c17aae13451b51098a3f8
Mustansir <me@mustansirg.in>
2023-03-12 06:58:09 +0530

    frontend work from backend guy

--- lib/pages/CompanyDetails/companyDetails.dart
+++ lib/pages/CompanyDetails/companyDetails.dart
@@ -26,7 +26,7 @@ class _CompanyDetailsState extends State<CompanyDetails> {
         children: [
           Container(
             child: Image.network(
-              "https://www.deccanherald.com/sites/dh/files/article_images/2018/06/04/file70frsj22cw4i6zyeaed.jpg",
+              'https://innovative-minds.mustansirg.in/${widget.details['company_headquarters']}',
               width: double.infinity,
               height: 220,
               fit: BoxFit.fill,
@@ -54,7 +54,7 @@ class _CompanyDetailsState extends State<CompanyDetails> {
                               children: [
                                 const SizedBox(height: 20),
                                 Text(
-                                  widget.details.company,
+                                  widget.details['company_name'],
                                   style: TextStyle(fontSize: 36),
                                 ),
                                 // const SizedBox(height: 20),
@@ -64,127 +64,132 @@ class _CompanyDetailsState extends State<CompanyDetails> {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     Text(
-                                      "Job Title: ${widget.details.title}",
+                                      "Job Title: ${widget.details['title']}",
                                       style: textTheme.headlineLarge,
                                     ),
                                     const SizedBox(
                                       height: 20,
                                     ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        Chip(
-                                          shape: const RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.all(
-                                                  Radius.circular(15))),
-                                          label: Row(
-                                            children: [
-                                              const Icon(
-                                                Icons.location_on,
-                                                color: AppColors.black,
-                                              ),
-                                              Text(
-                                                widget.details.location,
-                                                style: const TextStyle(
-                                                    color: AppColors.black),
-                                              ),
-                                            ],
-                                          ),
-                                          backgroundColor: const Color.fromARGB(
-                                              255, 160, 255, 164),
-                                          elevation: 6.0,
-                                          shadowColor: Colors.grey[60],
-                                          padding: const EdgeInsets.all(2),
-                                        ),
-                                        Chip(
-                                          shape: const RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.all(
-                                                  Radius.circular(15))),
-                                          label: Row(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.center,
-                                            children: [
-                                              const Icon(
-                                                Icons.work,
-                                                color: AppColors.black,
-                                              ),
-                                              Text(
-                                                widget.details.experience,
-                                                style: const TextStyle(
-                                                    color: AppColors.black),
-                                              )
-                                            ],
-                                          ),
-                                          backgroundColor: const Color.fromARGB(
-                                              255, 255, 156, 156),
-                                          // side: BorderSide(
-                                          //   width: 1,
-                                          //   color: Color.fromARGB(255, 73, 255, 82),
-                                          // ),
-                                          elevation: 6.0,
-                                          shadowColor: Colors.grey[60],
-                                          padding: const EdgeInsets.all(5),
-                                        ),
-                                        Chip(
-                                          shape: const RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.all(
-                                              Radius.circular(15),
+                                    SingleChildScrollView(
+                                      scrollDirection: Axis.horizontal,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Chip(
+                                            shape: const RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.all(
+                                                    Radius.circular(15))),
+                                            label: Row(
+                                              children: [
+                                                const Icon(
+                                                  Icons.location_on,
+                                                  color: AppColors.black,
+                                                ),
+                                                Text(
+                                                  widget.details['address'],
+                                                  style: const TextStyle(
+                                                      color: AppColors.black),
+                                                ),
+                                              ],
                                             ),
+                                            backgroundColor: const Color.fromARGB(
+                                                255, 160, 255, 164),
+                                            elevation: 6.0,
+                                            shadowColor: Colors.grey[60],
+                                            padding: const EdgeInsets.all(2),
                                           ),
-                                          label: Row(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.center,
-                                            children: [
-                                              const Icon(
-                                                Icons.currency_rupee,
-                                                color: Colors.black,
+                                          SizedBox(width: 10,),
+                                          Chip(
+                                            shape: const RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.all(
+                                                    Radius.circular(15))),
+                                            label: Row(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.center,
+                                              children: [
+                                                const Icon(
+                                                  Icons.work,
+                                                  color: AppColors.black,
+                                                ),
+                                                Text(
+                                                  widget.details['experience'],
+                                                  style: const TextStyle(
+                                                      color: AppColors.black),
+                                                )
+                                              ],
+                                            ),
+                                            backgroundColor: const Color.fromARGB(
+                                                255, 255, 156, 156),
+                                            // side: BorderSide(
+                                            //   width: 1,
+                                            //   color: Color.fromARGB(255, 73, 255, 82),
+                                            // ),
+                                            elevation: 6.0,
+                                            shadowColor: Colors.grey[60],
+                                            padding: const EdgeInsets.all(5),
+                                          ),
+                                          SizedBox(width: 10,),
+                                          Chip(
+                                            shape: const RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.all(
+                                                Radius.circular(15),
                                               ),
-                                              Text(
-                                                widget.details.pay,
-                                                style: const TextStyle(
-                                                    color: AppColors.black),
-                                              )
-                                            ],
+                                            ),
+                                            label: Row(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.center,
+                                              children: [
+                                                const Icon(
+                                                  Icons.currency_rupee,
+                                                  color: Colors.black,
+                                                ),
+                                                Text(
+                                                  widget.details['payscale'],
+                                                  style: const TextStyle(
+                                                      color: AppColors.black),
+                                                )
+                                              ],
+                                            ),
+                                            backgroundColor: const Color.fromARGB(
+                                                255, 255, 243, 156),
+                                            // side: BorderSide(
+                                            //   width: 1,
+                                            //   color: Color.fromARGB(255, 73, 255, 82),
+                                            // ),
+                                            elevation: 6.0,
+                                            shadowColor: Colors.grey[60],
+                                            padding: const EdgeInsets.all(5),
                                           ),
-                                          backgroundColor: const Color.fromARGB(
-                                              255, 255, 243, 156),
-                                          // side: BorderSide(
-                                          //   width: 1,
-                                          //   color: Color.fromARGB(255, 73, 255, 82),
-                                          // ),
-                                          elevation: 6.0,
-                                          shadowColor: Colors.grey[60],
-                                          padding: const EdgeInsets.all(5),
-                                        ),
-                                      ],
+                                        ],
+                                      ),
                                     ),
                                     const SizedBox(
                                       height: 10,
                                     ),
-                                    SingleChildScrollView(
-                                      scrollDirection: Axis.horizontal,
-                                      child: Wrap(
-                                        spacing: 10.0,
-                                        runSpacing: 2.0,
-                                        children: List<Widget>.generate(
-                                          widget.details.tags.length,
-                                          (int index) {
-                                            return Chip(
-                                              side: const BorderSide(
-                                                width: 1,
-                                                color: AppColors.secondary,
-                                              ),
-                                              label: Text(
-                                                  widget.details.tags[index],
-                                                  style: textTheme.bodyMedium),
-                                            );
-                                          },
-                                        ),
-                                      ),
-                                    ),
+                                    // SingleChildScrollView(
+                                    //   scrollDirection: Axis.horizontal,
+                                    //   child: Wrap(
+                                    //     spacing: 10.0,
+                                    //     runSpacing: 2.0,
+                                    //     children: List<Widget>.generate(
+                                    //       widget.details.tags.length,
+                                    //       (int index) {
+                                    //         return Chip(
+                                    //           side: const BorderSide(
+                                    //             width: 1,
+                                    //             color: AppColors.secondary,
+                                    //           ),
+                                    //           label: Text(
+                                    //               widget.details.tags[index],
+                                    //               style: textTheme.bodyMedium),
+                                    //         );
+                                    //       },
+                                    //     ),
+                                    //   ),
+                                    // ),
                                     const SizedBox(
                                       height: 5,
                                     ),
@@ -202,7 +207,7 @@ class _CompanyDetailsState extends State<CompanyDetails> {
                                       height: 10,
                                     ),
                                     Text(
-                                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                                      widget.details['description'],
                                       style: textTheme.bodyLarge!.copyWith(
                                         color: Colors.grey,
                                         height: 1.5,
@@ -259,7 +264,7 @@ class _CompanyDetailsState extends State<CompanyDetails> {
                               backgroundColor: Colors.white,
                               backgroundImage: NetworkImage(
                                 // width: constraints.maxWidth,
-                                widget.details.companyPhoto,
+                                "https://innovative-minds.mustansirg.in/${widget.details['company_logo']}",
                               ),
                             ),
                           ],
